4a02fc7a5ba08fb59a778c263cfe4105c2aeb4de
OMironyuk <1234m4321@mail.ru>
2021-07-04 23:01:00 +0300

    Update config.tf. Add variables

--- Lesson_14/Home_work/config.tf
+++ Lesson_14/Home_work/config.tf
@@ -7,12 +7,24 @@ terraform {
   }
 }
 
+variable "token" {
+  type    = string
+}
+variable "storage_access_key" {
+  type    = string
+}
+variable "storage_secret_key" {
+  type    = string
+}
+
 provider "yandex" {
-  token                    = "AQAAAAAEsphYAATuwWvGrL6H7U3xtjfKhT2St0I"
+  token                    = var.token
  # service_account_key_file = "path_to_service_account_key_file"
   cloud_id                 = "b1gchdap7uflt27e7238"
   folder_id                = "b1g0a0qj4eah8vmbgdpb"
   zone                     = "ru-central1-b"
+  storage_access_key       = var.storage_access_key
+  storage_secret_key       = var.storage_secret_key
 }
 resource "yandex_compute_instance" "build" {
   name        = "devs14-build"
@@ -54,12 +66,12 @@ resource "yandex_compute_instance" "build" {
       "sudo mkdir -p /home/user/ && cd /home/user/ && sudo git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git",
       "cd /home/user/boxfuse-sample-java-war-hello && sudo mvn package"
     ]
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
-    host     = "${yandex_compute_instance.build.network_interface.0.nat_ip_address}"
-  }
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host     = "${yandex_compute_instance.build.network_interface.0.nat_ip_address}"
+    }
   }
 }
 // Unmark if need to create network and subnet
@@ -73,3 +85,14 @@ resource "yandex_compute_instance" "build" {
 //  network_id = "${yandex_vpc_network.default.id}"
 //  v4_cidr_blocks = []
 //  }
+resource "yandex_storage_bucket" "bucket" {
+  bucket = "bucket-for-artifact"
+  acl = "private"
+}
+resource "yandex_storage_object" "cute-cat-picture" {
+  bucket = "bucket-for-artifact"
+  key    = "profile"
+  //source =  "terraform.tfstate"
+  //source = "ubuntu@${yandex_compute_instance.build.network_interface.0.nat_ip_address}:/home/user/boxfuse-sample-java-war-hello/target/hello-1.0.war"
+  source = "ubuntu@${yandex_compute_instance.build.network_interface.0.nat_ip_address}:/home/ubuntu/.ssh/.profile"
+}
