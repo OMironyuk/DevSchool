f361944c2d664edd686938170d787766f2302be6
OMironyuk <1234m4321@mail.ru>
2021-07-05 16:52:05 +0300

    Add create instance prod

--- Lesson_14/Home_work/config.tf
+++ Lesson_14/Home_work/config.tf
@@ -81,20 +81,12 @@ resource "yandex_compute_instance" "build" {
 //  network_id = "${yandex_vpc_network.default.id}"
 //  v4_cidr_blocks = []
 //  }
-//resource "null_resource" "copy_artifact" {
-//  provisioner "local-exec" {
-//    command = "scp ubuntu@${yandex_compute_instance.build.network_interface.0.nat_ip_address}:/home/user/boxfuse-sample-java-war-hello/target/hello-1.0.war /home/user/"
-//  }
-//  depends_on = [
-//    yandex_compute_instance.build
-//  ]
-//}
 
 resource "yandex_storage_bucket" "bucket" {
   bucket = "bucket-for-artifact"
   acl = "private"
 }
-resource "yandex_storage_object" "cute-cat-picture" {
+resource "yandex_storage_object" "art-object" {
   provisioner "local-exec" {
     command = "scp -o StrictHostKeyChecking=no ubuntu@${yandex_compute_instance.build.network_interface.0.nat_ip_address}:/home/user/boxfuse-sample-java-war-hello/target/hello-1.0.war /home/user/"
   }
@@ -104,6 +96,68 @@ resource "yandex_storage_object" "cute-cat-picture" {
   //source = "ubuntu@${yandex_compute_instance.build.network_interface.0.nat_ip_address}:/home/user/boxfuse-sample-java-war-hello/target/hello-1.0.war"
   source = "/home/user/hello-1.0.war"
   depends_on = [
+    yandex_compute_instance.build
+  ]
+}
+
+resource "yandex_compute_instance" "prod" {
+  name        = "devs14-prod"
+  platform_id = "standard-v2"
+  zone        = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd83klic6c8gfgi40urb"
+      size = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = "e2la11hhlvdpko78kgor"
+    // subnet_id = "${yandex_vpc_subnet.default-ru-central1-b.id}" - unmark if network and subnet are created
+    nat = true
+  }
+
+  metadata = {
+    foo = "bar"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y tomcat9",
+    ]
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host     = "${yandex_compute_instance.prod.network_interface.0.nat_ip_address}"
+    }
+  }
+}
+resource "null_resource" "copy_artifact" {
+  provisioner "file" {
+    source      = "/home/user/hello-1.0.war"
+    destination = "/var/lib/tomcat9/webapps/hello-1.0.war"
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host     = "${yandex_compute_instance.prod.network_interface.0.nat_ip_address}"
+    }
+  }
+   depends_on = [
     yandex_compute_instance.build
   ]
 }
